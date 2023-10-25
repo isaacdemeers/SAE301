63dfa0b868d31219551ee84214d8a636ca026226
Isrem_ <32309148+isaacdemeers@users.noreply.github.com>
2023-10-25 16:07:00 +0200

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,19 +5,35 @@ let M = {
   productCollection: new ProductCollection(),
 };
 
+await M.productCollection.loadProducts("http://localhost:8888/api/products");
+
 let V = {};
 
+V.init = function () {
+  document.body.addEventListener("click", C.clickHandler);
+}
+;
 V.render = function (data) {
   document.querySelector("#Products").innerHTML = productRenderer(data);
 };
 
 let C = {};
 
-C.init = async function () {
-  console.log(
-    await M.productCollection.loadProducts("http://localhost:8888/api/products")
-  );
+C.init = function () {
+  V.init();
   V.render(M.productCollection.getProducts());
+
+
+  
+};
+
+C.clickHandler = function (e) {
+  console.log(e.target);
+  if (e.target.classList.contains("filters__item")) {
+    let type = e.target.dataset.type;
+    console.log(type);
+  }
 };
 
 C.init();
+
