a16d3004581141d6d28b3b62e9efa54339db4ffb
Poldus <leopold.canou@gmail.com>
2023-10-29 14:01:21 +0100

    update burger menu

--- dist/js/burger.js
+++ dist/js/burger.js
@@ -1,11 +1,11 @@
-      var menuButton = document.getElementById("menuButton");
-      var burgerBg = document.querySelector(".burger__bg");
-      menuButton.addEventListener("click", function (e) {
-        menuButton.classList.toggle("is-active");
-        burgerBg.classList.toggle("burger__bg--active");
-        e.preventDefault();
-      });
+let burger = function (data) {
+  var menuButton = document.getElementById("menuButton");
+  var burgerBg = document.querySelector(".burger__bg");
+  menuButton.addEventListener("click", function (e) {
+    menuButton.classList.toggle("is-active");
+    burgerBg.classList.toggle("burger__bg--active");
+    e.preventDefault();
+  });
+};
 
-
-
-
+export { burger as burger };
